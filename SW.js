ad5940947c04a7bc15e249d3207f398d8ba788e1
Paschal-hub39 <azubuikepaschal6@gmail.com>
2026-03-19 17:50:08 +0100

    Update SW.js

--- SW.js
+++ SW.js
@@ -1,22 +1,16 @@
-const CACHE_NAME = "paschala-v3-online-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/admin.html",
-  "/manifest.json",
-  "/sw.js"
-];
-
-// Install Service Worker
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.skipWaiting();
 });
 
-// Fetch files from cache first, then online
+self.addEventListener("activate", event => {
+event.waitUntil(
+caches.keys().then(names => {
+return Promise.all(names.map(cache => caches.delete(cache)));
+})
+);
+self.clients.claim();
+});
+
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+event.respondWith(fetch(event.request));
 });
